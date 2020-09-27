c59cf2d48f60193dcdba146cb9c864e90c5e3d5f
Laowang <wsj6051@126.com>
2020-09-27 23:44:43 +0800

    add one file

---
+++ src/main/java/com/lw/oss/OSSTest.java
@@ -0,0 +1,10 @@
+package com.lw.oss;
+
+import com.aliyun.oss.OSSClient;
+
+public class OSSTest {
+    public static void main(String[] args) {
+//        OSSClient oSSClient = new OSSClient();
+//        oSSClient.listObjects();
+    }
+}
